2b8efa1a7335a43f46057855b5646814d8b70bbe
Saipranitha <kotipalli.saipranitha@gmail.com>
2024-03-22 23:00:58 +0530

    Create basic GUI without interaction

--- tool.py
+++ tool.py
@@ -2,7 +2,7 @@ import functions
 import time
 # from functions import get_todos, write_todos
 tim = time.strftime("%b %d,%Y %H:%M:%S")
-print("It is",tim)
+print("It is", tim)
 
 while True:
     user_action = input("type add , show ,edit,complete or exit: ")
