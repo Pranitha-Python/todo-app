729233d81ab014e4d9dfed46a378916c658cc1fd
Saipranitha <kotipalli.saipranitha@gmail.com>
2024-03-24 00:18:30 +0530

    Implemented the feet conversion method

--- feet gui.py
+++ feet gui.py
@@ -1,13 +1,27 @@
 import PySimpleGUI as sg
+from feetconverter import convert
 
 label1 = sg.Text("Enter feet:")
 label2 = sg.Text("Enter Inches:")
-feet = sg.InputText(tooltip="Enter feet: ")
-Inches = sg.InputText(tooltip="Enter Inches: ")
+feet = sg.InputText(tooltip="Enter feet: ", key="feet")
+Inches = sg.InputText(tooltip="Enter Inches: ", key="Inches")
 convert_button = sg.Button("Convert")
+output = sg.Text(key="output")
 
 window = sg.Window("Convertor", layout=[[label1, feet]
-                                     , [label2, Inches],
-                                       [convert_button]])
-window.read()
+                                       , [label2, Inches],
+                                       [convert_button] , [output]])
+
+while True:
+    event, values = window.read()
+    f = int(values['feet'])
+    i = int(values['Inches'])
+    output = convert(f, i)
+    window["output"].update(value=output)
+
+'''print(event, values)
+print(values)
+print(values['feet'])
+print(output)'''
+
 window.close()
--- parsers.py
+++ parsers.py
@@ -3,13 +3,13 @@ def parse(user_input):
     and return the two values via a dictionary.
     """
     # Get the two values in a list
-    parts = user_input.split(",")
+    parts = user_input.split(" ")
 
     # Store the two values in variables
-    lower_bound = int(parts[0])
-    upper_bound = int(parts[1])
+    feet = int(parts[0])
+    inches = int(parts[1])
 
     # Inject the values in a dictionary
-    return {"lower_bound": lower_bound, "upper_bound": upper_bound}
+    return {"feet": feet, "inches": inches}
 
 
